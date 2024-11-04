c5dc1e6dd3a3df144429e1574417a9d18815be0b
Dabeus99 <procameras22@gmail.com>
2024-11-04 15:44:21 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showit(var1) {
-document.getElementById('b1').style.display = 'none'
-document.getElementById('b2').style.display = 'none'
-document.getElementById('b3').style.display = 'none'
-document.getElementById('b4').style.display = 'none'
-
-document.getElementById(var1).style.display = 'block'
-}
