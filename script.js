4bc96aab4ef6829248d69a50a0de96e4bd7a9c8a
Dabeus99 <procameras22@gmail.com>
2024-12-05 18:04:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function showit(var1) {
+document.getElementById('b1').style.display = 'none'
+document.getElementById('b2').style.display = 'none'
+document.getElementById('b3').style.display = 'none'
+document.getElementById('b4').style.display = 'none'
+document.getElementById('b5').style.display = 'none'
 
+document.getElementById(var1).style.display = 'block'
+}
+
+function myfunction() {
+var1 = document.getElementById('things').value
+var2 = document.getElementById('things').value
+}
